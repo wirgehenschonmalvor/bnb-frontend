2798eef6b77151c284fd00d4a1cd957377b7e18e
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-01-24 21:35:06 +0100

    feat: 🎸 merge paragraph into branch

---
+++ src/components/bnb-paragraph/defines.ts
@@ -0,0 +1,13 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BNBParagraph
+ **/
+
+/**
+ * Props
+ */
+interface BnbParagraphProps {
+	leadtext: boolean;
+	capitalize: boolean;
+}
+
+export { BnbParagraphProps };
