fa3705b41c3456a02b9fb426b438a822ec5a3a0b
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-08 18:38:15 +0100

    add bnb-cms-info-box component

---
+++ src/components/cms/bnb-cms-info-box/template.ts
@@ -0,0 +1,11 @@
+import { html, createStyle, HTMLFragment } from '@biotope/element';
+import * as styles from './styles.scss';
+
+import { BnbCmsInfoBoxProps } from './defines';
+
+export const template = (data: BnbCmsInfoBoxProps): HTMLFragment => {
+	return html`
+		<slot />
+		${createStyle(styles)}
+	`;
+};
