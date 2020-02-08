6ff1c80c66db43cf3a7bb27979b5d3154b5d48d0
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-08 17:08:44 +0100

    add icon component

---
+++ src/components/bnb-icon/Icons/index.ts
@@ -0,0 +1,7 @@
+// import * as bullet from './Icons/bullet.svg';
+
+const icons = {
+	// bullet,
+};
+
+export default icons;
---
+++ src/components/bnb-icon/defines.ts
@@ -0,0 +1,12 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BnbIcon
+ **/
+
+/**
+ * Props
+ */
+interface BnbIconProps {
+	icon: string;
+}
+
+export { BnbIconProps };
