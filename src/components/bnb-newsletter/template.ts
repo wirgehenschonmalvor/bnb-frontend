cb84bb2137a13bd4957e89aff15bc2521e17d3dd
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-09 00:34:15 +0100

    styling newsletter component

--- src/components/bnb-newsletter/template.ts
+++ src/components/bnb-newsletter/template.ts
@@ -3,10 +3,19 @@ import * as styles from './styles.scss';
 
 import { BnbNewsletterProps } from './defines';
 
-
-export const template = ( data: BnbNewsletterProps ): HTMLFragment => {
-  return html`
-    <slot/>
-    ${createStyle(styles)}
-  `;
-}
+export const template = (data: BnbNewsletterProps): HTMLFragment => {
+	return html`
+		<div class="newsletter__wrapper">
+			<div class="newsletter-text">
+				<slot name="text" />
+			</div>
+			<div class="newsletter-form">
+				<slot name="form" />
+				<input type="text" />
+				<input type="text" />
+				<button>anmelden</button>
+			</div>
+		</div>
+		${createStyle(styles)}
+	`;
+};
