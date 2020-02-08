817e0beb8beb4f654b064542f67ad6328a83232c
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-08 23:01:48 +0100

    several changes on headline color

--- src/components/bnb-bullet-list/template.ts
+++ src/components/bnb-bullet-list/template.ts
@@ -5,7 +5,7 @@ import { BnbBulletListProps } from './defines';
 
 export const template = (data: BnbBulletListProps): HTMLFragment => {
 	return html`
-		<bnb-headline level="4"><slot /></bnb-headline>
+		<bnb-headline white level="4"><slot /></bnb-headline>
 		<div class="bullet-list__wrapper"><slot name="list" /></div>
 		${createStyle(styles)}
 	`;
