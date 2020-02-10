b502277a1384a39ae23f51d394ff4992486a2e6b
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-10 23:25:41 +0100

    styling bullet list

--- src/components/cms/bnb-cms-info-box/bnb-cms-info-box.ts
+++ src/components/cms/bnb-cms-info-box/bnb-cms-info-box.ts
@@ -2,15 +2,15 @@ import Component, { HTMLFragment } from '@biotope/element';
 import { template } from './template';
 import { BnbCmsInfoBoxProps } from './defines';
 
-class BnbCmsInfoBox extends Component< BnbCmsInfoBoxProps, null > {
-  public static componentName = 'bnb-cms-info-box';
-  public static attributes = [];
-  
-  protected readonly defaultProps: BnbCmsInfoBoxProps = {};
+class BnbCmsInfoBox extends Component<BnbCmsInfoBoxProps, null> {
+	public static componentName = 'bnb-cms-info-box';
+	public static attributes = [];
 
-  public render(): HTMLFragment {
-    return template( { ...this.props });
-  }
+	protected readonly defaultProps: BnbCmsInfoBoxProps = {};
+
+	public render(): HTMLFragment {
+		return template({ ...this.props });
+	}
 }
 
 export default BnbCmsInfoBox;
