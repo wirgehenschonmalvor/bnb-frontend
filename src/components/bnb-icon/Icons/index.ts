687b6e0d6a246f13b2b5197a7d208ad5626f0da0
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-10 21:55:44 +0100

    optimze and load icons

--- src/components/bnb-icon/Icons/index.ts
+++ src/components/bnb-icon/Icons/index.ts
@@ -1,7 +1,31 @@
-// import * as bullet from './icons/bullet.svg';
+import * as share from './svg/share.svg';
+import * as email from './svg/email.svg';
+import * as facebook from './svg/facebook.svg';
+import * as linkedin from './svg/linkedin.svg';
+import * as twitter from './svg/twitter.svg';
+import * as xing from './svg/xing.svg';
+import * as close from './svg/close.svg';
+import * as search from './svg/search.svg';
+import * as play from './svg/movie.svg';
+import * as playYoutube from './svg/play_youtube.svg';
+import * as calendar from './svg/calendar.svg';
+import * as clock from './svg/clock.svg';
+import * as quote from './svg/quote.svg';
 
 const icons = {
-	// 	bullet,
+	share,
+	email,
+	facebook,
+	linkedin,
+	twitter,
+	xing,
+	close,
+	search,
+	play,
+	playYoutube,
+	calendar,
+	clock,
+	quote,
 };
 
 export default icons;
