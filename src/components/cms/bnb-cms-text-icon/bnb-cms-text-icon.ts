eb3561d6b539c9cb6aff0fa0e7e9f8d9bc8baf47
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-14 00:06:25 +0100

    feat: 🎸 sett new headlines

--- src/components/cms/bnb-cms-text-icon/bnb-cms-text-icon.ts
+++ src/components/cms/bnb-cms-text-icon/bnb-cms-text-icon.ts
@@ -2,12 +2,17 @@ import Component, { HTMLFragment } from '@biotope/element';
 import { template } from './template';
 import { BnbCmsTextIconProps } from './defines';
 import BnbIcon from '../../bnb-icon/bnb-icon';
+import BnbHeadline from '../../bnb-headline/bnb-headline';
+import BnbButtonCTA from '../../bnb-button-cta/bnb-button-cta';
 
 class BnbCmsTextIcon extends Component<BnbCmsTextIconProps, null> {
 	public static componentName = 'bnb-cms-text-icon';
-	public static attributes = [];
 
-	static dependencies = [BnbIcon as typeof Component];
+	public static dependencies = [
+		BnbIcon as typeof Component,
+		BnbHeadline as typeof Component,
+		BnbButtonCTA as typeof Component,
+	];
 	protected readonly defaultProps: BnbCmsTextIconProps = {};
 
 	public render(): HTMLFragment {
