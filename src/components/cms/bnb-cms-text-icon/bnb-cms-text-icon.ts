123f99ec17245ab47620297fc5b0ced3fd9331b8
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-14 21:03:51 +0100

    feat: 🎸 refactoring text-icon component

--- src/components/cms/bnb-cms-text-icon/bnb-cms-text-icon.ts
+++ src/components/cms/bnb-cms-text-icon/bnb-cms-text-icon.ts
@@ -7,13 +7,22 @@ import BnbButtonCTA from '../../bnb-button-cta/bnb-button-cta';
 
 class BnbCmsTextIcon extends Component<BnbCmsTextIconProps, null> {
 	public static componentName = 'bnb-cms-text-icon';
+	public static attributes = [
+		'icon',
+		'heading',
+		'color',
+		{ name: 'button', type: 'object' },
+	];
 
 	public static dependencies = [
 		BnbIcon as typeof Component,
 		BnbHeadline as typeof Component,
 		BnbButtonCTA as typeof Component,
 	];
-	protected readonly defaultProps: BnbCmsTextIconProps = {};
+	protected readonly defaultProps: BnbCmsTextIconProps = {
+		icon: '',
+		heading: '',
+	};
 
 	public render(): HTMLFragment {
 		return template({ ...this.props });
--- src/components/cms/bnb-cms-text-icon/template.ts
+++ src/components/cms/bnb-cms-text-icon/template.ts
@@ -5,18 +5,32 @@ import { BnbCmsTextIconProps } from './defines';
 
 export const template = (data: BnbCmsTextIconProps): HTMLFragment => {
 	return html`
-		<slot name="heading" />
+		<bnb-headline level="3" color="${data.color}"
+			>${data.heading}</bnb-headline
+		>
 		<div class="text-icon">
 			<div class="text-icon--left">
-				<slot />
+				<bnb-headline level="2" color="${data.color}"
+					><slot
+				/></bnb-headline>
 			</div>
 			<div class="text-icon--right">
-				<bnb-icon icon="iconset-spacing-right"></bnb-icon>
+				<bnb-icon icon="${data.icon}"></bnb-icon>
 			</div>
 		</div>
-		<div class="button">
-			<slot name="button" />
-		</div>
+		${data.button
+			? html`
+					<div class="button">
+						<bnb-button-cta
+							version="${data.button.version}"
+							value="${data.button.value}"
+							url="${data.button.url}"
+							target="${data.button.target}"
+						>
+						</bnb-button-cta>
+					</div>
+			  `
+			: ''}
 		${createStyle(styles)}
 	`;
 };
