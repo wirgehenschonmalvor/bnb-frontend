30c95bb0e67cc9821a4d79c3b2fbd87ee6aa57c3
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-19 21:50:11 +0100

    feat: 🎸 refactor bnb-newsletter component

--- src/components/bnb-newsletter/bnb-newsletter.ts
+++ src/components/bnb-newsletter/bnb-newsletter.ts
@@ -1,16 +1,29 @@
 import Component, { HTMLFragment } from '@biotope/element';
 import { template } from './template';
 import { BnbNewsletterProps } from './defines';
+import BnbInput from '../bnb-input/bnb-input';
+import BnbHeadline from '../bnb-headline/bnb-headline';
+import BnbCheckbox from '../bnb-checkbox/bnb-checkbox';
+import BnbButtonCTA from '../bnb-button-cta/bnb-button-cta';
+import BnbParagraph from '../bnb-paragraph/bnb-paragraph';
 
-class BnbNewsletter extends Component< BnbNewsletterProps, null > {
-  public static componentName = 'bnb-newsletter';
-  public static attributes = [];
-  
-  protected readonly defaultProps: BnbNewsletterProps = {};
+class BnbNewsletter extends Component<BnbNewsletterProps, null> {
+	public static componentName = 'bnb-newsletter';
+	public static attributes = [];
 
-  public render(): HTMLFragment {
-    return template( { ...this.props });
-  }
+	protected readonly defaultProps: BnbNewsletterProps = {};
+
+	public static dependencies = [
+		BnbInput as typeof Component,
+		BnbHeadline as typeof Component,
+		BnbCheckbox as typeof Component,
+    BnbButtonCTA as typeof Component,
+    BnbParagraph as typeof Component,
+	];
+
+	public render(): HTMLFragment {
+		return template({ ...this.props });
+	}
 }
 
 export default BnbNewsletter;
--- src/components/bnb-newsletter/template.ts
+++ src/components/bnb-newsletter/template.ts
@@ -5,16 +5,42 @@ import { BnbNewsletterProps } from './defines';
 
 export const template = (data: BnbNewsletterProps): HTMLFragment => {
 	return html`
-		<div class="newsletter__wrapper">
-			<div class="newsletter-text">
-				<slot name="text" />
-			</div>
-			<div class="newsletter-form">
-				<slot name="form" />
-				<input type="text" />
-				<input type="text" />
-				<button>anmelden</button>
-			</div>
+		<bnb-headline level="3">anmelden</bnb-headline>
+		<div class="newsletter-form">
+			<bnb-input
+				type="text"
+				label="email"
+				required
+				placeholder="Emailadresse"
+			></bnb-input>
+			<bnb-input
+				type="text"
+				label="first name"
+				placeholder="Vorname"
+			></bnb-input>
+			<bnb-input
+				type="text"
+				label="last name"
+				placeholder="Nachname"
+			></bnb-input>
+			<bnb-checkbox label="accept data protection" required link>
+				<bnb-paragraph
+					>Ich habe die
+					<bnb-link
+						value="Datenschutzbestimmunge"
+						url="data-protection.html"
+						target="_blank"
+					></bnb-link>
+					und die
+					<bnb-link
+						value="Nutzungsbedingungen"
+						url="data-protection.html"
+						target="_blank"
+					></bnb-link>
+					gelesen und akzeptiert.*</bnb-paragraph
+				>
+			</bnb-checkbox>
+			<bnb-button-cta version="primary" value="Anmelden"></bnb-button-cta>
 		</div>
 		${createStyle(styles)}
 	`;
