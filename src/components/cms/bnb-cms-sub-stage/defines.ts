69c2feee47bfbad9f65f8a548536bfaa9874f80b
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-19 01:42:43 +0100

    feat: 🎸 add bnb-cms-sub-stage component

---
+++ src/components/cms/bnb-cms-sub-stage/defines.ts
@@ -0,0 +1,18 @@
+import { BnbImageProps } from "../../bnb-image/defines";
+
+/**
+ *  ## DEFINE ALL INTERFACES FOR BnbCmsSubStage
+ **/
+
+/**
+ * Props
+ */
+interface BnbCmsSubStageProps {
+  heading: string;
+	image: BnbImageProps;
+}
+
+
+export {
+  BnbCmsSubStageProps
+}
