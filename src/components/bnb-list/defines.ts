972c853448058b60e1e6753c07ec747f8dddb4bd
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-23 19:53:40 +0100

    feat: 🎸 adding list and list-item component

---
+++ src/components/bnb-list/defines.ts
@@ -0,0 +1,14 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR BnbList
+ **/
+
+/**
+ * Props
+ */
+interface BnbListProps {
+	ordered?: boolean;
+	counterPrefix?: number;
+	counterStart?: number;
+}
+
+export { BnbListProps };
