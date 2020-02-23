abd0abc68640153c69708f81b2e396156b40a018
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-23 19:54:10 +0100

    fix: 🐛 refactor partner component

--- src/components/cms/bnb-cms-partner/template.ts
+++ src/components/cms/bnb-cms-partner/template.ts
@@ -5,18 +5,20 @@ import { BnbCmsPartnerProps } from './defines';
 
 export const template = (data: BnbCmsPartnerProps): HTMLFragment => {
 	return html`
-		<bnb-headline level="3">
-			${data.heading}
-		</bnb-headline>
-		<div class="partner-container">
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
-			<bnb-icon icon="bildmarke"></bnb-icon>
+		<div class="component-wrapper">
+			<bnb-headline level="3">
+				${data.heading}
+			</bnb-headline>
+			<div class="partner-container">
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+				<bnb-icon icon="bildmarke"></bnb-icon>
+			</div>
 		</div>
 		${createStyle(styles)}
 	`;
