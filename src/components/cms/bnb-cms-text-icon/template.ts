067b5d79dce0f29638ca1836377b4733675b849a
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-23 19:54:47 +0100

    feat: 🎸 reafactor text-icon component to work with lists

--- src/components/cms/bnb-cms-text-icon/template.ts
+++ src/components/cms/bnb-cms-text-icon/template.ts
@@ -5,36 +5,41 @@ import { BnbCmsTextIconProps } from './defines';
 
 export const template = (data: BnbCmsTextIconProps): HTMLFragment => {
 	return html`
-		${data.heading ? html`
-		<bnb-headline level="3" color="${data.color}"
-			>${data.heading}</bnb-headline
-		>` : ''}
+		${data.heading
+			? html`
+					<bnb-headline level="3" color="${data.color}"
+						>${data.heading}</bnb-headline
+					>
+			  `
+			: ''}
 		<div class="text-icon">
 			<div class="text-icon--left">
 				${data.copyText
 					? html`
-							<bnb-paragraph><slot /></bnb-paragraph>
+							<bnb-paragraph><slot name="text" /></bnb-paragraph>
 					  `
 					: html`
-				<bnb-headline level="2" color="${data.color}"
-					><slot
-				/></bnb-headline>`}
+							<bnb-headline level="2" color="${data.color}"
+								><slot name="headline"
+							/></bnb-headline>
+							<bnb-paragraph><slot name="text"/></bnb-paragraph>
+					  `}
+				${data.button
+					? html`
+							<bnb-button-cta
+								version="${data.button.version}"
+								value="${data.button.value}"
+								url="${data.button.url}"
+								target="${data.button.target}"
+							>
+							</bnb-button-cta>
+					  `
+					: ''}
 			</div>
 			<div class="text-icon--right">
 				<bnb-icon icon="${data.icon}"></bnb-icon>
 			</div>
 		</div>
-		${data.button
-			? html`
-			<bnb-button-cta
-				version="${data.button.version}"
-				value="${data.button.value}"
-				url="${data.button.url}"
-				target="${data.button.target}"
-			>
-			</bnb-button-cta>
-			  `
-			: ''}
 		${createStyle(styles)}
 	`;
 };
