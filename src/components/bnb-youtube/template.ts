220c57a2b9484c712320f6212a0b0ca02c820e85
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-26 00:53:04 +0100

    refactor: 💡 refactor bnb-youtube

--- src/components/bnb-youtube/template.ts
+++ src/components/bnb-youtube/template.ts
@@ -1,11 +1,36 @@
 import { html, createStyle, HTMLFragment } from '@biotope/element';
 import * as styles from './styles.scss';
 
-import { BnbYoutubeProps } from './defines';
+import { BnbYoutubeProps, BnBYoutubeMethods } from './defines';
 
-export const template = (data: BnbYoutubeProps): HTMLFragment => {
+export const template = (
+	data: BnbYoutubeProps & BnBYoutubeMethods,
+	refs,
+): HTMLFragment => {
 	return html`
+		${data.thumbnail
+			? html`
+					${createStyle(`
+        :host img {
+          object-fit: ${data.thumbnail.objectFit};
+          object-position: ${data.thumbnail.objectPosition};
+        }`)}
+					<img
+						ref=${refs.thumbnail}
+						src="${data.thumbnail.src}"
+						srcset="${data.thumbnail.srcset}"
+						sizes="${data.thumbnail.sizes}"
+						alt="${data.thumbnail.title}"
+					/>
+					<span
+					ref=${refs.button} onclick=${data.onButtonClick}>
+						<bnb-icon icon="play">
+						</bnb-icon>
+					</span>
+			  `
+			: ''}
 		<iframe
+			ref=${refs.video}
 			src=${`https://www.youtube.com/embed/${data.youtubeId}?modestbranding=true&showinfo=false&rel=0`}
 			frameborder="0"
 			allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
