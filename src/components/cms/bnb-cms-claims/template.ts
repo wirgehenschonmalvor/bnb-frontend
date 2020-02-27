a917542343ed36c4116bc989ff1ff4662b700942
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-27 01:18:46 +0100

    fix: 🐛 fix indentation and background on accordion and claims
    ✅ Closes: #103

--- src/components/cms/bnb-cms-claims/template.ts
+++ src/components/cms/bnb-cms-claims/template.ts
@@ -5,11 +5,15 @@ import { BnbCmsClaimsProps } from './defines';
 
 export const template = (data: BnbCmsClaimsProps): HTMLFragment => {
 	return html`
-		<div class="claim-wrapper">
-		<bnb-headline level="3">${data.heading}</bnb-headline>
-		<bnb-headline class="h1" level="1">${data.headline}</bnb-headline>
-		<bnb-paragraph><slot name="intro"/></bnb-paragraph>
-		<slot name="claims" />
+		<div class="component-wrapper">
+			<div class="claim-wrapper">
+				<bnb-headline level="3">${data.heading}</bnb-headline>
+				<bnb-headline class="h1" level="1"
+					>${data.headline}</bnb-headline
+				>
+				<bnb-paragraph><slot name="intro"/></bnb-paragraph>
+				<slot name="claims" />
+			</div>
 		</div>
 		${createStyle(styles)}
 	`;
