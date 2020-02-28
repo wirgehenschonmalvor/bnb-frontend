88f4d90cfc88187c7b0eba1278ca8309aaeab902
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-02-28 21:57:12 +0100

    fix: 🐛 requested changes

--- src/components/cms/bnb-cms-footer/template.ts
+++ src/components/cms/bnb-cms-footer/template.ts
@@ -50,7 +50,7 @@ export const template = (data: BnbCmsFooterProps): HTMLFragment => {
 				</div>
 				<div class="footer-links--middle">
 					<bnb-paragraph class="headline"
-						>So erreichst duuns:</bnb-paragraph
+						>So erreichst du uns:</bnb-paragraph
 					>
 					<bnb-paragraph class="contact">
 						Brand New Bundestag e.V. <br />
