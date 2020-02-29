6cbce8684fd6fc560ddcfb520f731a59685238e5
Robin Spielmann <robin.spielmann@virtual-identity.com>
2020-02-29 14:48:17 +0100

    feat: 🎸 add newsletter mail chimp integration

--- src/components/bnb-newsletter/template.ts
+++ src/components/bnb-newsletter/template.ts
@@ -5,43 +5,86 @@ export const template = (): HTMLFragment => {
 	return html`
 		<bnb-headline level="3">anmelden</bnb-headline>
 		<div class="newsletter-form">
-			<bnb-input
-				type="text"
-				label="email"
-				required
-				placeholder="E-Mail"
-			></bnb-input>
-			<bnb-input
-				type="text"
-				label="first name"
-				placeholder="Vorname"
-			></bnb-input>
-			<bnb-input
-				type="text"
-				label="last name"
-				placeholder="Nachname"
-			></bnb-input>
-			<bnb-checkbox
-				id="acceptDataProtection"
-				label="accept data protection"
-				required
-				link
+			<!-- Begin Mailchimp Signup Form -->
+			<form
+				action="https://brandnewbundestag.us4.list-manage.com/subscribe/post?u=84c699733b9aa82c84722c5e8&amp;id=d4e718f10b"
+				method="post"
+				id="mc-embedded-subscribe-form"
+				name="mc-embedded-subscribe-form"
+				class="validate"
+				target="_blank"
+				novalidate
 			>
-				<bnb-paragraph
-					>Ich habe die
-					<bnb-link
-						value="Datenschutzbestimmungen"
-						url="data-protection.html"
-						target="_blank"
-					></bnb-link>
-					gelesen und akzeptiert.*</bnb-paragraph
-				>
-			</bnb-checkbox>
-			<bnb-button-cta
-				version="primary"
-				value="Anmelden"
-				tab-index="0"
-			></bnb-button-cta>
+				<div>
+					<div class="input__wrapper">
+						<input
+							type="email"
+							name="EMAIL"
+							class="required email"
+							id="mce-EMAIL"
+							placeholder="E-Mail*"
+							required
+						/>
+						<label for="mce-EMAIL"
+							>Email Address <span class="asterisk">*</span>
+						</label>
+					</div>
+					<div class="input__wrapper">
+						<input
+							type="text"
+							name="FNAME"
+							id="mce-FNAME"
+							placeholder="Vorname"
+						/>
+						<label for="mce-FNAME">Vorname</label>
+					</div>
+					<div class="input__wrapper">
+						<input
+							type="text"
+							value=""
+							name="LNAME"
+							id="mce-LNAME"
+							placeholder="Nachname"
+						/>
+						<label for="mce-LNAME">Nachname</label>
+					</div>
+					<div id="mce-responses" class="clear">
+						<div
+							class="response"
+							id="mce-error-response"
+							style="display:none"
+						></div>
+						<div
+							class="response"
+							id="mce-success-response"
+							style="display:none"
+						></div>
+					</div>
+					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div
+						style="position: absolute; left: -5000px;"
+						aria-hidden="true"
+					>
+						<input
+							type="text"
+							name="b_84c699733b9aa82c84722c5e8_d4e718f10b"
+							tabindex="-1"
+							value=""
+						/>
+					</div>
+					<div class="clear">
+						<input
+							type="submit"
+							value="Anmelden"
+							name="subscribe"
+							id="mc-embedded-subscribe"
+							class="button"
+						/>
+					</div>
+				</div>
+			</form>
+
+			<!--End mc_embed_signup-->
 		</div>
 		${createStyle(styles)}
 	`;
