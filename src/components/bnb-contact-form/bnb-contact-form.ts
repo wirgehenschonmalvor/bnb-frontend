0b8d78217c54f81f9445732392bcf4c53447e261
Karl Baumhauer <karl.baumhauer@virtual-identity.com>
2020-03-24 18:18:52 +0100

    fix: 🐛 fix conflicts

--- src/components/bnb-contact-form/bnb-contact-form.ts
+++ src/components/bnb-contact-form/bnb-contact-form.ts
@@ -1,15 +1,8 @@
 import Component, { HTMLFragment } from '@biotope/element';
 import { template } from './template';
-import BnbLink from '../bnb-link/bnb-link';
-import BnbParagraph from '../bnb-paragraph/bnb-paragraph';
 
 class BnbContactForm extends Component< null, null > {
   public static componentName = 'bnb-contact-form';
-
-  public static dependencies = [
-    BnbLink as typeof Component,
-    BnbParagraph as typeof Component,
-  ]
 
   public render(): HTMLFragment {
     return template();
